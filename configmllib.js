13ccfbe2ae1510e831f10a88129b1971933b3be3
aquilax <aquilax@gmail.com>
2014-09-20 08:34:04 +0200

    Moved sample config to tests

--- configmllib.js
+++ configmllib.js
@@ -1,54 +1,6 @@
 'use strict';
 
-var config = {
-	data: [
-	{
-		type: 'execute',
-		value: 'pathogen#infect()'
-	}, {
-		type: 'set',
-		name: 't_Co',
-		value: 256,
-	}, {
-		type: 'switch',
-		name: 'syntax',
-		value: 'on'
-	}, {
-		type: 'switch',
-		name: 'colorscheme',
-		value: 'haelequin'
-	}, {
-		type: 'set',
-		name: 'tabstop',
-		value: 4,
-	}, {
-		type: 'set',
-		name: 'langmap',
-		value: 'ъy,оo,пp,дd,гg,ГG,аa,АA,мm'
-	}, {
-		type: 'map',
-		name: '<F2>',
-		value: ':NERDTreeToggle<CR>'
-	}, {
-		type: 'let',
-		name: 'g:airline_left_sep',
-		value: '\'\''
-	}, {
-		type: 'let',
-		name: 'g:airline_right_sep',
-		value: '\'\''
-	}, {
-		type: 'switch',
-		name: 'autocmd',
-		value: 'Filetype php setlocal ts=4 sts=4 sw=4 expandtab'
-	}, {
-		type: 'switch',
-		name: 'autocmd', 
-		value: 'Filetype yaml setlocal ts=2 sts=2 sw=2'
-	}
-	]
-};
-
+var config = require('./syntax/vim/tests/01/config.json');
 var template = require('./syntax/vim/syntax.json');
 
 
